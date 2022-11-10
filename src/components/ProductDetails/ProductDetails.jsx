e68d64d7a3498744bfbfab065323ca96c50fed29
Cristopher <cristopher.rubio6397@alumnos.udg.mx>
2022-11-09 22:56:18 -0600

    fix: stuyles in product details

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -23,23 +23,23 @@ const ProductDetails = ({ productId }) => {
     context.setLoadingStatus(false)
   }, [productId])
   return (
-    <Container fluid className='product-details px-5 mt-5'>
+    <Container fluid className='product-details m-3'>
       {context.loadingStatus
         ? <Loading />
         : (
-          <Card className='card-product-details'>
+          <Card border='light' className='card-product-details'>
             <Card.Header as='h5'>{product.product_name}</Card.Header>
             <Card.Body>
               <Row>
-                <div className='col-4 align-self-center'>
+                <div className='col-5 align-self-center'>
                   <Card.Img
                     fluid
                     className='product-image'
                     src={product.image}
                   />
                 </div>
-                <div className='col-8'>
-                  <Card.Title className='fs-4'>Overview</Card.Title>
+                <div className='col-7'>
+                  <Card.Title className='card-product-details__title mb-5'>Details</Card.Title>
                   <Card.Text>
                     {product.description}
                   </Card.Text>
@@ -47,7 +47,7 @@ const ProductDetails = ({ productId }) => {
                     Brand: {product.brand}
                   </Card.Text>
                   <Card.Title className='fs-4' id='product-price'>${product.price} MXN</Card.Title>
-                  <Card.Title className='fs-4'>Free shipping!</Card.Title>
+                  <Card.Title className='card-product-details__shipping fs-5 '>Free shipping!</Card.Title>
                   <Card.Text>
                     <i className='bi bi-truck' style={{ color: 'rgb(0, 10, 59)', fontSize: '20px' }} />
                     Estimated delivery on <b>Dec 16</b>
@@ -60,6 +60,9 @@ const ProductDetails = ({ productId }) => {
                             <>
                               <Button variant='danger' size='sm' disabled>Buy now</Button>{' '}
                               <Button variant='warning' size='sm' disabled>Add to cart</Button>{' '}
+                              <Card.Text className='text-muted'>
+                                Please log in or sign up to enjoy all the features we offer!
+                              </Card.Text>
                             </>
                             )
                           : (
